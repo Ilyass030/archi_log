9f2c6ec334b93db7bb17120acc3c1f2202bc1c94
Remmane <ilyass.remmane@edu.esiee.fr>
2025-06-24 10:48:48 +0200

    update database sqlite

--- src/modele.py
+++ src/modele.py
@@ -1,132 +1,132 @@
-import mysql.connector
+import sqlite3
 
 def create_list():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
-    mycursor = mydb.cursor()
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
 
-    mycursor.execute('''create table if not exists liste_genres(
-                        id int auto_increment primary key, nom varchar(20))''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS liste_genres (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nom TEXT UNIQUE
+        )
+    ''')
 
-    mycursor.execute('''create table if not exists liste_films(
-                        id int auto_increment primary key,nom varchar(50), resume varchar(500),
-                        annee_sortie unsigned int, nb_visionnage unsigned int,
-                        nb_notes unsigned int, note_moyenne unsined char''')
+    genres = [
+        "Action", "Aventure", "Animation", "Comédie", "Crime", "Documentaire",
+        "Drama", "Famille", "Fantasy", "Histoire", "Horreur", "Musique",
+        "Mystère", "Romance", "Thriller", "Film télé", "Guerre", "Western"
+    ]
+
+    for genre in genres:
+        cursor.execute("INSERT OR IGNORE INTO liste_genres (nom) VALUES (?)", (genre,))
+
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS liste_films (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom TEXT NOT NULL,
+                resume TEXT,
+                annee_sortie INTEGER,
+                nb_visionnage INTEGER DEFAULT 0,
+                nb_notes INTEGER DEFAULT 0,
+                note_moyenne REAL DEFAULT 0.0
+            )
+        ''')
     
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (1, "Action"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (2, "Aventure"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (3, "Animation"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (4, "Comédie"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Crime"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Documentaire"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Drama"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Famille"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Fantasy"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Histoire"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Horreur"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Musique"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Mystère"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Romance"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Thriller"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Film télé"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Guerre"))
-    mycursor.execute('''insert ignore into liste_genres (id, nom) values(%s, %s)''', (5, "Western"))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-    mydb.commit()
-    mycursor.close()
 
-def list():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
-    mycursor = mydb.cursor()
 
-    mycursor.execute('''select j.nom, p.nom, prix, t.nom, description from liste_jeux j, liste_genres p, liste_type t where j.plateforme_id=p.id and j.type_id=t.id''')
-    etuds = mycursor.fetchall()
-    print(etuds)
-    mycursor.close()
-    return(etuds)
 
-def type():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
-    mycursor = mydb.cursor()
+# def list():
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="leane0208",
+#         database="archi_logicielle"
+#     )
+#     mycursor = mydb.cursor()
 
-    mycursor.execute('''select * from liste_type''')
-    etuds = mycursor.fetchall()
-    print(etuds)
-    mycursor.close()
-    return(etuds)
+#     mycursor.execute('''select j.nom, p.nom, prix, t.nom, description from liste_jeux j, liste_genres p, liste_type t where j.plateforme_id=p.id and j.type_id=t.id''')
+#     etuds = mycursor.fetchall()
+#     print(etuds)
+#     mycursor.close()
+#     return(etuds)
 
-def plateforme():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
-    mycursor = mydb.cursor()
+# def type():
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="leane0208",
+#         database="archi_logicielle"
+#     )
+#     mycursor = mydb.cursor()
 
-    mycursor.execute('''select * from liste_genres''')
-    etuds = mycursor.fetchall()
-    print(etuds)
-    mycursor.close()
-    return(etuds)
+#     mycursor.execute('''select * from liste_type''')
+#     etuds = mycursor.fetchall()
+#     print(etuds)
+#     mycursor.close()
+#     return(etuds)
 
-def add_type(type):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
+# def plateforme():
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="leane0208",
+#         database="archi_logicielle"
+#     )
+#     mycursor = mydb.cursor()
 
-    mycursor = mydb.cursor()
-    insert = 'insert into liste_type(nom) values("' + type + '")'
-    mycursor.execute(insert)
-    mydb.commit()
+#     mycursor.execute('''select * from liste_genres''')
+#     etuds = mycursor.fetchall()
+#     print(etuds)
+#     mycursor.close()
+#     return(etuds)
 
-    mycursor.close() 
+# def add_type(type):
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="leane0208",
+#         database="archi_logicielle"
+#     )
 
-def add_plateforme(plateforme):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
+#     mycursor = mydb.cursor()
+#     insert = 'insert into liste_type(nom) values("' + type + '")'
+#     mycursor.execute(insert)
+#     mydb.commit()
 
-    mycursor = mydb.cursor()
-    insert = 'insert into liste_genres(nom) values("' + plateforme + '")'
-    mycursor.execute(insert)
-    mydb.commit()
+#     mycursor.close() 
 
-    mycursor.close()
+# def add_plateforme(plateforme):
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="leane0208",
+#         database="archi_logicielle"
+#     )
 
-def add_jeux(nom, plateforme, prix, type, description):
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="leane0208",
-        database="archi_logicielle"
-    )
+#     mycursor = mydb.cursor()
+#     insert = 'insert into liste_genres(nom) values("' + plateforme + '")'
+#     mycursor.execute(insert)
+#     mydb.commit()
 
-    mycursor = mydb.cursor()
-    insert='''insert into liste_jeux(nom, plateforme_id, prix, type_id, description) values(%s, %s, %s, %s, %s)'''
-    val = (nom, plateforme, prix, type, description)
-    mycursor.execute(insert, val)
-    mydb.commit()
+#     mycursor.close()
 
-    mycursor.close()
-    return(list())
+# def add_jeux(nom, plateforme, prix, type, description):
+#     mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="leane0208",
+#         database="archi_logicielle"
+#     )
+
+#     mycursor = mydb.cursor()
+#     insert='''insert into liste_jeux(nom, plateforme_id, prix, type_id, description) values(%s, %s, %s, %s, %s)'''
+#     val = (nom, plateforme, prix, type, description)
+#     mycursor.execute(insert, val)
+#     mydb.commit()
+
+#     mycursor.close()
+#     return(list())
